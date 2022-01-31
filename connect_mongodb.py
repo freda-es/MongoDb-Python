63f7c1c9ee65f0b39d82e9482ac2fd65cf9e08f1
FredaDyrkaj <86465451+freda-es@users.noreply.github.com>
2022-01-31 12:31:35 +0100

    Create connect_mongodb.py

---
+++ connect_mongodb.py
@@ -0,0 +1,19 @@
+import urllib
+import certifi
+from pymongo import MongoClient
+from unicodedata import name
+
+#connect mongodb
+def connect_mongodb (db_username, db_passw, db_name):
+    MONGODB_USERNAME = urllib.parse.quote_plus(db_username)
+    MONGODB_PASSWORD = urllib.parse.quote_plus(db_passw)
+    MONGODB_DATABASE = db_name
+    ca = certifi.where()
+    MONGODB_URL = "mongodb+srv://"+MONGODB_USERNAME+":"+MONGODB_PASSWORD+"@cluster0.ks8wq.mongodb.net/"+MONGODB_DATABASE+"?retryWrites=true&w=majority"
+    client = MongoClient(MONGODB_URL, tlsCAFile=ca)
+    database = client.get_database(db_name)
+    return database
+#call the function to generate your own db
+databases = connect_mongodb (db_username='freda', db_passw='19freda87', db_name='dbECommerce')
+#generate cluster
+my_cluster = databases.users
